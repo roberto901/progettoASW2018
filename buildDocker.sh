fec5920b29f69a29f0d9ab9d2680fae506825f08
RobertoFrancazi <robertofranc@icloud.com>
2018-06-27 16:36:50 +0200

    Update buildDocker.sh

--- buildDocker.sh
+++ buildDocker.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-eval $(minikube docker-env)
-docker build -t sentence-service-img sentence-service
-docker build -t word-service-img word-service
+docker build --rm -t sentence-service-img ./sentence-service
+docker build --rm -t word-service-img ./word-service
